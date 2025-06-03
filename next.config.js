3df6956dab95a0d61bcfcaa02153a9f10b8b058b
dyyfk <879608095@qq.com>
2025-06-03 01:19:55 -0700

    revert

--- next.config.js
+++ next.config.js
@@ -1,29 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'dyyfk.github.io',
-      },
-    ],
   },
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  basePath: process.env.NODE_ENV === 'production' ? '/Scent_Emitter' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Scent_Emitter/' : '',
   trailingSlash: true,
 };
 
