3cadde5f06c601b41214438f4ba2f97d7ca0fb9c
dyyfk <879608095@qq.com>
2025-06-03 01:13:10 -0700

    changed image path

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dyyfk.github.io',
+      },
+    ],
   },
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/Scent_Emitter' : '',
